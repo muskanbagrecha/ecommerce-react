852b7b8ac7eced7e0f61174802c72d02486c0d15
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-03-21 15:29:14 +0530

    updated db

---
+++ src/Assets/Images/image.js
@@ -0,0 +1,45 @@
+import makeup from "./Beauty/makeup.webp";
+import beauty1 from "./Beauty/beauty1.webp";
+import beauty2 from "./Beauty/beauty2.webp";
+import beauty3 from "./Beauty/beauty3.webp";
+import bags from "./Bags/bags.webp";
+import bags1 from "./Bags/bags1.webp";
+import bags2 from "./Bags/bags2.webp";
+import bags3 from "./Bags/bags3.webp";
+import gowns from "./Gowns/gowns.webp";
+import gowns1 from "./Gowns/gowns1.webp";
+import gowns2 from "./Gowns/gowns2.webp";
+import gowns3 from "./Gowns/gowns3.webp";
+import jewellery from "./Jewellery/jewellery.webp";
+import jewellery1 from "./Jewellery/jewellery1.webp";
+import jewellery2 from "./Jewellery/jewellery2.webp";
+import jewellery3 from "./Jewellery/jewellery3.webp";
+import women from "./Women/women.webp";
+import women1 from "./Women/women1.webp";
+import women2 from "./Women/women2.webp";
+import women3 from "./Women/women3.webp";
+import women4 from "./Women/women4.webp";
+
+export {
+  makeup,
+  beauty1,
+  beauty2,
+  beauty3,
+  bags,
+  bags1,
+  bags2,
+  bags3,
+  gowns,
+  gowns1,
+  gowns2,
+  gowns3,
+  jewellery,
+  jewellery1,
+  jewellery2,
+  jewellery3,
+  women,
+  women1,
+  women2,
+  women3,
+  women4,
+};
