2a03ef5500e6b67d7bae733d575f559efb8988a0
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-03-21 22:02:03 +0530

    minor fix

--- src/Assets/Icons/icons.jsx
+++ src/Assets/Icons/icons.jsx
@@ -53,7 +53,7 @@ function Eye(props) {
   );
 }
 
-export function eyeClose(props) {
+export function EyeClose(props) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}>
       <path
@@ -64,4 +64,4 @@ export function eyeClose(props) {
   );
 }
 
-export { Search, Cart, Wishlist, User, Eye, eyeClose };
+export { Search, Cart, Wishlist, User, Eye, EyeClose };
