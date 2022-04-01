62961c2d4e3c6e9ba3b764083706dcb443a6ea97
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-04-01 09:26:19 +0530

    added cart functionality

---
+++ src/Pages/Cartpage/CouponContainer.jsx
@@ -0,0 +1,13 @@
+export const CouponContainer = () => {
+  return (
+    <div className="coupons__container">
+      <h5>Coupons</h5>
+      <div className="CTA__container">
+        <button className="btn btn-outline full-width">
+          <i className="fa-solid fa-tag"></i>
+          <span> Apply Coupon</span>
+        </button>
+      </div>
+    </div>
+  );
+};
